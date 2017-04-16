34dfc166adcb0b6da6b2f388693df47bb804a0af
titer1 <titer1@qq.com>
2017-04-16 10:18:10 +0800

    Update reverse-nodes-in-k-group.java
     //{1,2} 2 can not pass
    您的代码已保存
    运行超时:您的程序未能在规定时间内运行结束，请检查是否循环有错或算法复杂度过大。
    case通过率为0.00%

--- reverse-nodes-in-k-group.java
+++ reverse-nodes-in-k-group.java
@@ -29,47 +29,53 @@ For k = 3, you should return:3->2->1->4->5
  *     }
  * }
  */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+
 public class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         
         int cnt = 0 ;
-        //if（k< 0 )
-            
+
         //if (k == 0)
-        if( k<= 1 )
+        if( k<= 1 ||(head == null) || (head.next == null))
             return head;
-        //if()  // max list node
-        
-        //no node
-        if(head == null)
-            return null;
-        //node 1 only
-        if(head.next == null)
-            return head;
-        
+ 
         //node 2 only
         ListNode pre = head ;
         ListNode now = head.next;
         ListNode more = now.next;
         ListNode oldH = head;
         
+        
         if(more == null)//2 node
         {
+            //java.lang.System.out.printfln(pre.val+" "+now.val);
             if(k == 2)
             {
                 now.next = pre;
                 head = now;
                 return head;
             }
+            else
+                return null;//add it here later
             
         }
         
-      
+        /*
         while(more != null)
          {
-            
-            //nextNode.next = nextNode;
-           // nextN
             
             more = now.next;
             now.next = pre;
@@ -86,9 +92,9 @@ public class Solution {
             
         }
         
-        
+        */
         return pre;
-        
+        //{1,2} 2
         
     }
 }
