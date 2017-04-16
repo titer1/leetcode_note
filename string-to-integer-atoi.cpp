fedbb4d61df03875f76a03290bfe03e71fbfbb49
titer1 <titer1@qq.com>
2017-04-16 10:49:57 +0800

    Update string-to-integer-atoi.cpp
            //give 1 ,output 0

--- string-to-integer-atoi.cpp
+++ string-to-integer-atoi.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     int isdigit(char a)
     {
-        if( a>='0' && a<='9')//ascii 0x31
+        if( (a>='0') && (a<='9'))//ascii 0x31
             return 1;
         else
             return 0;
@@ -22,7 +22,7 @@ public:
         //int len = strlen(str);//not \0 endding
         
         char *move = (char*)str;
-        while( (*move++) == ' ');//spaces
+        while( (*move++) != ' ');//ignore spaces
         
         int flag_minus = 1;
         if(*move == '-')
@@ -37,9 +37,10 @@ public:
         while( isdigit(*move) )//deal all digit, else meet char
         {
             arrDigit[len++] = *move - '0';
+            move++;
             
         }
-        if(len == 0)//|| len>10) //geek tips 2147483647 only 10 digits
+        if(len == 0)//|| len>10) 
             return 0;
         for(int j = len -1  ;j>=0; j--)
         {
@@ -52,11 +53,14 @@ public:
         //judge
         ret = flag_minus*ret;
         return ret;
-        
-          
+
+            
         
     }
-        
+        //geek tips 2147483647 only 10 digits
+    //您的程序发生段错误，可能是数组越界，堆栈溢出（比如，递归调用层数太多）等情况引起
+        //while dead loop
+    
     	// 100
     // >bigger int32 2^32 INT_MAX (2147483647) 
     // -100
