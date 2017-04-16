4a2f2201602419951aeaa72003d7b2a8eeafc20a
titer1 <titer1@qq.com>
2017-04-16 10:41:26 +0800

    Create string-to-integer-atoi.cpp
    init

---
+++ string-to-integer-atoi.cpp
@@ -0,0 +1,69 @@
+#include <math.h>
+#define INT_MAX (2147483647)
+#define INT_MIN (-2147483648)
+class Solution {
+public:
+    int isdigit(char a)
+    {
+        if( a>='0' && a<='9')//ascii 0x31
+            return 1;
+        else
+            return 0;
+        
+    }
+
+   
+    int atoi(const char *str) {
+        //[space][-][digit]
+        
+        if(str == null) return 0;
+        //ignore the space
+        
+        int len = strlen(str);//not \0 endding
+        
+        char *move = str;
+        while( (*move++) == ' ');//spaces
+        
+        int flag_minus = 1;
+        if（*move == '-')
+        {
+            flag_minus = -1;
+            move ++;
+        }
+        
+        int arrDigit[32]={0};
+        int len = 0;
+        long long ret = 0;
+        while( isdigit(*move) )//deal all digit, else meet char
+        {
+            arrDigit[len++] = *move - '0';
+            
+        }
+        if(len == 0)//|| len>10) //geek tips 2147483647 only 10 digits
+            return 0;
+        for(int j = len -1  ;j>=0; j--)
+        {
+            ret += arrDigit[j]*pow(10,len-1-j);
+            if((flag_minus==1)&& (ret>INT_MAX))
+                return INT_MAX;
+            if((flag_minus!=1)&& (ret<INT_MIN))
+                return INT_MIN;
+        }
+        //judge
+        ret = flag_minus*ret;
+        return ret;
+        
+            
+        
+        
+    }
+        
+    	// 100
+    // >bigger int32 2^32 INT_MAX (2147483647) 
+    // -100
+    	// 100.1 //endwith charcater
+    // abc123
+    // 123abc   0 
+    // 空格123
+    //
+};
