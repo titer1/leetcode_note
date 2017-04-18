5524a3387b1cc994ea9ee3354cb6e4f56dc6de60
titer1 <titer1@qq.com>
2017-04-18 17:56:44 +0800

    Create atoi_basic.cpp
    init ,alaways report isdigit fail
    
    ref more: https://www.nowcoder.com/profile/463120/codeBookDetail?submissionId=10039997

---
+++ atoi_basic.cpp
@@ -0,0 +1,83 @@
+#include <math.h>
+//#define INT_MAX (2147483647)
+//#define INT_MIN (-2147483648)
+class Solution {
+public:
+    int isdigit(char a)
+    {
+    //if( *str>='0' && *str<='9') {  
+        if( (a>='0') && (a<='9'))//ascii 0x31
+            return 1;
+        else
+            return 0;
+        
+    }
+
+   
+    int atoi(const char *str) {
+        //[space][-][digit]
+        
+        if(str == NULL) return 0;
+        //ignore the space
+        
+        //int len = strlen(str);//not \0 endding
+        
+        char *move = (char*)str;
+        while( (*move++) == ' ');//ignore spaces
+        
+        int flag_minus = 1;
+        if(*move == '-')
+        {
+            flag_minus = -1;
+            move ++;
+        }
+        else if(*move == '+')
+         {
+            flag_minus = 1;
+            move ++;
+        }
+        
+        
+        int arrDigit[32]={0};
+        int len = 0;
+        long long ret = 0;
+        
+        //return *move;
+        //return isdigit(*move);//debug 
+        
+        while( isdigit(*move) ==1)//deal all digit, else meet char
+        {
+            arrDigit[len++] = *move - '0';
+            move++;
+            
+        }
+        
+        if(len == 0)//|| len>10) 
+            return 0;
+        
+        for(int j = len -1  ;j>=0; j--)
+        {
+            ret += arrDigit[j]*pow(10,len-1-j);
+            if((flag_minus==1)&& (ret>INT_MAX))
+                return INT_MAX;
+            if((flag_minus!=1)&& (ret<INT_MIN))
+                return INT_MIN;
+        }
+        //judge
+        ret = flag_minus*ret;
+        return ret;
+        //give 1 ,output 0
+            
+        
+    }
+      //while dead loop
+    
+    	// 100
+    // >bigger int32 2^32 INT_MAX (2147483647) 
+    // -100
+    	// 100.1 //endwith charcater
+    // abc123
+    // 123abc   0 
+    // 空格123
+    //
+};
